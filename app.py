e0cf202d4787b31a68bdb55e667e4899e81fdc2d
Aram Morcecian <amorcecian@MacBook-Pro-de-Melanie.local>
2019-09-01 17:55:12 -0300

    Counting how many bikes are rented from all the stations

--- app.py
+++ app.py
@@ -14,15 +14,23 @@ db = 'pfidb'
 
 #Create connection to DB
 conn = pymssql.connect(server,user,password,db)
-c = conn.cursor()
-c.execute("""SELECT * FROM [dbo].[estaciones-de-bicicletas-publicas]""")
-for row in c.fetchall():
-    print('id: '+ str(row[0])+'   Nombre: '+str(row[1]))
+query = """SELECT * FROM [dbo].[estaciones-de-bicicletas-publicas]"""
+df_estaciones = pd.read_sql(query,conn)
+#print(df_estaciones.head())
 
 #Read CSV to df
-df = pd.read_csv("recorridos-realizados-2018.csv")
-print(df.head())
+df_recorridos = pd.read_csv("recorridos-realizados-2018.csv")
+#print(df.head())
 
+# Joining DFs
+df_inner = pd.merge(df_estaciones, df_recorridos, how='inner', left_on=['nro_est'], right_on=['bici_estacion_origen'])
+
+print(df_inner[['nombre','nro_est','bici_estacion_origen','bici_estacion_destino','bici_tiempo_uso']].head(10))
+
+df = df_inner.groupby('nombre')['nombre'].count().sort_values()
+print(df)
+
+# print(df_inner.groupby(['nombre'])['nombre'].count())
 
 conn.close()
 #Import CSV to DB
