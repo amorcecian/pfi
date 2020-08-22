cab6ffc457db4b5facbe942ef4655f1e2af5c4cd
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2020-08-22 20:54:29 -0300

    Moved config to the real file, as we seem to like naming everything config.py

--- data_bike/config.py
+++ data_bike/config.py
@@ -8,12 +8,22 @@ else:
     config_data['DEBUG'] = False
 
 config_data['API_TOKEN'] = getenv('API_TOKEN')
+config_data['AUTHENTICATION_MODE'] = 'MasterUser'
+config_data['SCOPE'] = ['https://analysis.windows.net/powerbi/api/.default']
+config_data['AUTHORITY'] = 'https://login.microsoftonline.com/organizations'
+
 if getenv('SERVER') is not None:
     config_data['SERVER'] = getenv('SERVER')
     config_data['USER'] = getenv('DB_USER')
     config_data['PASSWORD'] = getenv('PASSWORD')
     config_data['DB'] = getenv('DB')
     config_data['QUEUE'] = getenv('QUEUE')
+    config_data['WORKSPACE_ID'] = getenv('WORKSPACE_ID')
+    config_data['REPORT_ID'] = getenv('REPORT_ID')
+    config_data['CLIENT_ID'] = getenv('CLIENT_ID')
+    config_data['POWER_BI_USER'] = getenv('POWER_BI_USER')
+    config_data['POWER_BI_PASS'] = getenv('POWER_BI_PASS')
+
 else:
     # Local DB PC
     config_data['SERVER'] = 'DESKTOP-3OHRULK'
