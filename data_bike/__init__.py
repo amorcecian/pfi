1d7126ed1765b459d4beade9619fe19aea95c995
Aram Morcecian <amorcecian@gmail.com>
2020-08-22 21:38:27 -0300

    Debugging v2

--- data_bike/__init__.py
+++ data_bike/__init__.py
@@ -4,6 +4,7 @@ from config import config_data
 app = Flask(__name__)
 app.config.from_object('config')
 for k,v in config_data.items():
+    print(f'Setting {k}:{v}')
     app.config[k] = v
 
 from data_bike import routes
