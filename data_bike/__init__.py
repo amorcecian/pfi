918140e845090d70955a33982197da34102fd05d
Aram Morcecian <amorcecian@gmail.com>
2020-08-22 21:27:22 -0300

    Trying to solve the auth error

--- data_bike/__init__.py
+++ data_bike/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import config_data
 
 app = Flask(__name__)
 app.config.from_object('config')
+for k,v in config_data.items():
+    app.config[k] = v
 
 from data_bike import routes
